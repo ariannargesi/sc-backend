ab11ace2a7ff329270d3a2f1b475cbcb6295e1ba
arian <ariannargesi@gmail.com>
2020-08-22 00:44:45 +0430

    fsadf

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ const schema = require('./schema')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
-mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err, result) => {
-    if(err)
-    console.log(err)
-})
-const db = mongoose.connection
-db.on('err', () => {
-    console.log('error in connection')
-})
-db.once('open', () => {
-    console.log('database is ready')
-})
+// mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err, result) => {
+//     if(err)
+//     console.log(err)
+// })
+// const db = mongoose.connection
+// db.on('err', () => {
+//     console.log('error in connection')
+// })
+// db.once('open', () => {
+//     console.log('database is ready')
+// })
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
