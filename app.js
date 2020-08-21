4deebfb4529d738905a563c06a3f34e51d441b25
arian <ariannargesi@gmail.com>
2020-08-22 00:55:00 +0430

    fix

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 
 const { dbUrl } = require('./config')
-// const schema = require('./schema')
+const schema = require('./schema')
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
-// mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err, result) => {
-//     if(err)
-//     console.log(err)
-// })
-// const db = mongoose.connection
-// db.on('err', () => {
-//     console.log('error in connection')
-// })
-// db.once('open', () => {
-//     console.log('database is ready')
-// })
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err, result) => {
+    if(err)
+    console.log(err)
+})
+const db = mongoose.connection
+db.on('err', () => {
+    console.log('error in connection')
+})
+db.once('open', () => {
+    console.log('database is ready')
+})
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
@@ -35,4 +35,8 @@ app.post('/new', (req, res)  => {
     feedback.save().then(() => {
         res.send({ result: true})
     })
+})
+
+app.get('/', (req, res) => {
+ res.send({status: true})   
 })
