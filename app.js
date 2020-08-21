2779d2b4b747020e51a493228a0fcd06a8800a7b
arian <ariannargesi@gmail.com>
2020-08-21 23:42:14 +0430

    initial

---
+++ app.js
@@ -0,0 +1,32 @@
+const app = require('express')()
+const mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+
+const { dbUrl, port } = require('./config')
+const schema = require('./schema')
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+mongoose.connect(dbUrl, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('err', () => {
+    console.log('error in connection')
+})
+db.once('open', () => {
+    console.log('database is ready')
+})
+
+app.listen(port, () => {
+    console.log('app is running on port')
+})
+
+app.post('/new', (req, res)  => {
+    const content = req.body.feedback 
+    const date = new Date().getDate
+    const feedback = new schema({content, date})
+    feedback.save().then(() => {
+        res.send({ result: true})
+    })  
+})
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    dbUrl: 'mongodb+srv://admin:hVxp6WeHWE9w5KNE@flerbo-sv8cb.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    port: 8000
+}
